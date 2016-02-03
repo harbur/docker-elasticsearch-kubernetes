f7e3b8b6bb3740978a70adaae5ac1536a98393a9
spiddy <d.kapanidis@gmail.com>
2016-02-03 18:45:06 +0100

    Added NODE_NAME variable

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export CLUSTER_NAME=${CLUSTER_NAME:-elasticsearch-default}
+export NODE_NAME=${NODE_NAME:-$HOSTNAME}
 export NODE_MASTER=${NODE_MASTER:-true}
 export NODE_DATA=${NODE_DATA:-true}
 export HTTP_ENABLE=${HTTP_ENABLE:-true}
